7065b81cbd584a705d6bdcce391534aa4251c910
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-09-09 12:16:50 -0700

    added heroku link

--- src/components/NewReview.js
+++ src/components/NewReview.js
@@ -10,7 +10,7 @@ function NewReview({ pokemon_id }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch(`https://pokemon-moringa.herokuapp.com/reviews`, {
+    /*fetch(`https://pokemon-moringa.herokuapp.com/reviews`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
