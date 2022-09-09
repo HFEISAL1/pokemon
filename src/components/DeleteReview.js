c5f6d5933176c2620f18b3f4578a9071d26cadc8
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-09-09 12:06:53 -0700

    add edit review function

--- src/components/DeleteReview.js
+++ src/components/DeleteReview.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-/*function DeleteReview({ reviews_id }) {
+function DeleteReview({ reviews_id }) {
   const { id } = useParams();
 
   function handleDelete(e) {
