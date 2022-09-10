8b5fed9195f2a49e823fff093ba26ec31b468138
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-09-10 13:23:37 -0700

    added description of pokemon

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,10 @@ function Home() {
   return (
     <div className="comment">
       <h1>POKEMON REVIEW NOSTALGIA</h1>
+      <p>
+      Pokémon is a Japanese franchise that has been around for more than 20 years (2021 will mark its 25th anniversary!). Short for “pocket monsters,” Pokémon exists as a TV show, a trading card game, video games and toys. The Pokémon themselves are creatures that live in the wild or with their owner (as in your kid), referred to as their trainer. The trainer’s goal is to “catch them all” and, by all, we mean all 800+ of them.
+      Each Pokemon has a type (there are 11 in the trading card game), such as Fire, Water, Psychic, Metal or Dragon. Each Pokémon card will also indicate how “evolved” the character is—whether it’s in its basic form, Stage One form or Stage Two form—in the upper left-hand corner. Those shiny cards your kids probably want most are the Legendary Pokémon. They don’t evolve and are some of the most powerful cards in the trading card game, and they’re also only found in booster packs
+      </p>
       <img
         src="https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg"
         alt=""
